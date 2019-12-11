d09cdf21046b373bbc69bc77e735352b6cef9be7
Alvin Ng <alvin.cl.ng@gmail.com>
2019-12-11 12:45:59 -0800

    New update

--- js/main.js
+++ js/main.js
@@ -107,4 +107,24 @@ $(document).ready(function() {
     $("#vegas-prev").click(function() {
        $('#page-welcome').vegas('previous');
     });
+
+    $('#riptide .image-container').on('mouseenter mouseleave', function() {
+        console.log("hello")
+        $('#riptide .project-description').toggleClass('pt-7')
+    });
+
+    $('.hover-button').click(function(){
+        $('.image-container').toggleClass('hover-off');
+        $('.help').toggleClass('hide')
+        if($(this).hasClass('turned-off')) {
+            $(this).text('Hover Off').toggleClass('turned-off');
+        } else {
+            $(this).text('Hover On').toggleClass('turned-off');
+        }
+    })
+
+     $('.hover-button').on('mouseenter mouseleave', function(){
+         $('.help').toggleClass('show');
+     })
 });
+
